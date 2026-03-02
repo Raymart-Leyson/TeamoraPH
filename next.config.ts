116ce86f3a285ee14fd5a39d5b9703f2386d37c8
Raymart Leyson <raymart.leyson.rl@gmail.com>
2026-03-02 18:13:48 +0800

    hot-fix v1.5 1

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import type { NextConfig } from "next";
 import "./src/env";
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    root: __dirname,
+  },
   images: {
     remotePatterns: [
       {
