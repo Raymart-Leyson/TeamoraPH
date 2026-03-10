d8861f54a7a60cc04e1f4435f08f4638428d3c46
Raymart Leyson <raymart.leyson.rl@gmail.com>
2026-03-10 16:20:02 +0800

    Sample

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import type { NextConfig } from "next";
 import "./src/env";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
