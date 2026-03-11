91ceb6376642c3becff6a2518b7ed974b5e28188
Raymart Leyson <raymart.leyson.rl@gmail.com>
2026-03-11 21:18:01 +0800

    add logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Teamora - Remote Job Marketplace",
   description: "Find Top Remote Roles & Candidates around the globe.",
+  icons: {
+    icon: "/logo.png",
+  },
 };
 
 export default function RootLayout({
